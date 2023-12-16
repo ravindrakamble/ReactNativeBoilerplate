6c7f64c900ccaeb237ad01dc70078b891bbb4c06
Ravindra Kambale <ravindrakamble4u@gmail.com>
2023-12-16 16:54:29 +0530

    updated for Quotation app

--- android/app/src/main/java/com/quotation/MainActivity.java
+++ android/app/src/main/java/com/quotation/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativeboilerplate;
+package com.quotation;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/quotation/MainApplication.java
+++ android/app/src/main/java/com/quotation/MainApplication.java
@@ -1,4 +1,4 @@
-package com.reactnativeboilerplate;
+package com.quotation;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
